490f28a219841eeaddaa3d1157a340ebc5351e23
Alex-E-Camacho <alex.e.camacho@gmail.com>
2017-08-08 11:11:23 -0400

    #right_to_left_diagonal_winner? correctly finds winning marks

--- tic_tac_toe_board.rb
+++ tic_tac_toe_board.rb
@@ -33,7 +33,27 @@ class TTTBoard
     answer = false
     board_range = @board.count - 1
     diagonal_marks = (0..board_range).collect { |i| @board[i][i] }
-    diagonal_marks.uniq.count == 1 ? answer = true : false 
+    diagonal_marks.uniq.count == 1 ? answer = true : answer
+    answer
+  end
+
+  def right_to_left_diagonal_winner?
+    answer = false
+    # find the index to begin with by fnding the length minus 1
+    beginning_index = @board.count - 1
+    # find how many times to do the iteration
+    iterations = @board.count
+    # collect each mark in an array
+    diagonal_marks = []
+    # subtract 1 from the beginning index number while adding one to the row number [+1][-1]
+    row = 0
+    iterations.times do
+      diagonal_marks << @board[row][beginning_index]
+      row +=1
+      beginning_index -= 1
+    end
+    # check array with .uniq
+    diagonal_marks.uniq.count == 1 ? answer = true : answer
     answer
   end
 end
