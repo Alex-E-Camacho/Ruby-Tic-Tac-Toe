11c7275ae85e76e6bde50dbe05703ba069ebe3c2
Alex-E-Camacho <alex.e.camacho@gmail.com>
2017-08-08 23:21:30 -0400

    Change sleep speed

--- runner.rb
+++ runner.rb
@@ -4,13 +4,13 @@ puts "Press 1 to be X's or 2 to be O's."
 
 players_choice = gets.chomp
 
-if players_choice == "1"
-  player_mark = "X"
-  computer_mark = "O"
-else
-  player_mark = "O"
-  computer_mark = "X"
-end
+  if players_choice == "1"
+    player_mark = "X"
+    computer_mark = "O"
+  else
+    player_mark = "O"
+    computer_mark = "X"
+  end
 
 game = Game.new(computer_mark: computer_mark)
 
@@ -28,11 +28,11 @@ until game.over == true
       break
     end
 
-  sleep(2)
+  sleep(1.5)
 
   puts "Now the computer will choose."
 
-  sleep(1.5)
+  sleep(2)
 
   game.computer_move
 
